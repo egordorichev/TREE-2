6eafe4db894c9281f5dcc8a1f861ae8deae923f6
RamiLego4Game <ramilego4game@gmail.com>
2017-11-09 20:05:36 +0200

    Finished chips bootloading

--- BootLoader/init.lua
+++ BootLoader/init.lua
@@ -1,5 +1,25 @@
 --The bootloader of TREE-2
 
+--[[
+#Special Events:
+
+- Chip:PreInitialize, Triggered after loading the chips, with 2 arguments passed:
+-- APIS: All chips APIS merged in one table.
+-- DevKits: The devkits of all Chips in one table.
+
+- Chip:PostInitialize, Triggered after Chip:PostInitialize, with 2 arguments passed:
+-- APIS: All chips APIS merged in one table.
+-- DevKits: The devkits of all Chips in one table.
+
+#Devkits:
+
+Devkits are a table that can be returned by a Chip, which contains special functions for other chips to use.
+]]
+
+
+--Requirements
+local events = require("Engine.events")
+
 print("--==Bootloader Start==--")
 
 print("Compiling Chips Loaders...")
@@ -12,6 +32,40 @@ for _,ChipName in ipairs(love.filesystem.getDirectoryItems("/Chips/")) do
 end
 
 --Step 2: Initialize the chips.
-local config = require("BootLoader.config")
+local Config = require("BootLoader.config")
+local DevKits, APIS = {}, {}
+
+--Config verification
+if not Config.Chips then error("Config.Chips is not defined !!!") end
+
+--Chips Loading
+for id,Chip in ipairs(Config.Chips) do
+  local CName = Chip.Chip
+  
+  if not ChipsLoaders[CName] then error(CName.." Chip doesn't exist !") end
+  
+  print(CName.."-Chip","Load")
+  
+  local API, APINames, DevKit = ChipsLoaders[CName](Chip)
+  if API and APINames then
+    for _, AName in ipairs(APINames) do
+      APIS[AName] = API
+    end
+  end
+  
+  if Devkit then
+    DevKits[CName] = DevKit
+  end
+end
+
+--Chips Pre-Initialization
+print("Pre Initialize Chips")
+
+events:triggerEvent("Chip:PreInitialize",APIS,DevKits)
+
+--Chips Post-Initialization
+print("Post Initialize Chips")
+
+events:triggerEvent("Chip:PostInitialize",APIS,DevKits)
 
 print("--==Bootloader End==--")
