f920519f03b8aea38307f7a45d995bd1f0c615d8
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-11-10 20:51:09 +0200

    Re-wrote the RAM

--- Chips/RAM.lua
+++ Chips/RAM.lua
@@ -1,70 +1,104 @@
 --[[
 --==Contributers==--
-=- Egor Dorichev (egordorichev)
+- Rami Sabbagh (RamiLego4Game)
+- Egor Dorichev (egordorichev)
+]]
+
+--[[
+#Events:
+- 'RAM:poke' (address, value, oldvalue): Triggered when the memory is poked.
 ]]
 
 --The RAM CHIP
 
-return function(config)
+local function tohex(v) return string.format("0x%X",v) end
+
+--Value, expected Type, Variable Name
+local function Verify(v,t,n)
+  if type(v) ~= t then
+    error(n.." should be a "..t.." provided: "..type(v),3)
+  end
+end
+
+return function(Config)
+  
   local devkit = {} -- The RAM devkit
   local api = {} -- The RAM API
 
   local ram = {} -- RAM array
-  local ramSize = 10000 -- Tmp value, must be replaced with real one
-  -- once the real RAM layout is ready
-
-  -- Init RAM array
-  for i = 0, ramSize - 1 do
-    ram[i] = 0
+  local ramSize = Config.Size -- The current ram size.
+  
+  --Build the binary ram.
+  for addr=0,ramSize-1 do
+    ram[addr] = 0
   end
-
-  --  Checks, if RAM address is invalid
-  local function addrIsInvalid(addr)
-    return not addr or addr < 0 or addr >= ramSize
+  
+  --Helping functions
+  local function VerifyAddress(addr,name)
+    addr = floor(addr)
+    
+    if addr < 0 or addr > ramSize-1 then
+      error(name.." is out of range ("..addr.."), must be [0,"..(ramSize-1).."]",3)
+    end
+    
+    return addr
+  end
+  
+  local function VerifyValue(value)
+    value = floor(value)
+    
+    if value < 0 or value > 255 then
+      error("Value is out of range ("..value.."), must be [0,255]",3)
+    end
+    
+    return value
   end
 
   -- Returns byte from RAM at given position
-  function ram.peek(addr)
-    if addrIsInvalid(addr) then
-      return 0
-    end
-
-    return ram[math.floor(addr)]
+  function api.peek(addr)
+    Verify(addr,"number","Address")
+    
+    addr = VerifyAddress(addr,"Address")
+    
+    return ram[addr]
   end
 
   -- Sets RAM slot at given position to value
-  function ram.poke(addr, value)
-    if addrIsInvalid(addr) then
-      return
-    end
-
-    ram[math.floor(addr)] = math.floor(addr % 256) -- Make sure, value
-    -- is a number from 0 to 255
+  function api.poke(addr, value)
+    Verify(addr,"number","Address")
+    Verify(value,"number","Value")
+    
+    addr = VerifyAddress(addr,"Address")
+    value = VerifyValue(value)
+    events:triggerEvent("RAM:poke",addr,value,ram[addr])
+    
+    ram[addr] = value
   end
 
   -- Returns one bit from RAM at given position
-  function ram.peek1(addr)
+  function api.getBit(addr,bn)
     -- TODO
   end
 
   -- Sets one bit in RAM at given position
-  function ram.poke1(addr, value)
+  function api.setBit(addr,bn,value)
     -- TODO
   end
-
-  -- Inverts byte in RAM piat given position
-  function ram.invert(addr)
-    -- TODO
-    -- Can be used in graphics
+  
+  -- Get RAM at given interval
+  function api.memget(addr, length)
+  
   end
 
   -- Sets RAM at given interval to given value
-  function ram.memset(addr, value, length)
+  function api.memset(addr, value, length)
     -- TODO
   end
 
   -- Copies part of the RAM to another
-  function ram.memcpy(to, from, length)
+  function api.memcpy(to, from, length)
     -- TODO
   end
+  
+  return api, {"RAM"}, devkit
 end
