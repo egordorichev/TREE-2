11f6708b9ffa7aa1efffa2fc8b4156ca94af00d0
trelemar <trelemar@gmail.com>
2017-11-10 21:28:33 -0500

    Fixed monospacing with font and typo

--- Chips/Graphics.lua
+++ Chips/Graphics.lua
@@ -331,7 +331,7 @@ return function(Config)
     c = api.color(c)
 
     -- TODO: print it
-    -- Requires trelear's font
+    -- Requires trelemar's font
   end
 
   return api, {"Graphics"}, devkit
