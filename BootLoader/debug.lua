d706e04f526a1aae7d452a6615ca670448bd97ee
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-11-12 17:18:40 +0200

    Added some draw calls

--- BootLoader/debug.lua
+++ BootLoader/debug.lua
@@ -13,3 +13,15 @@ for Y=0,319 do
     RAM.poke(A, Pattern)
   end
 end
+
+local G = APIS.Graphics
+
+G.clear()
+
+G.rect(10,10,100,100, true, true)
+G.rect(10,150,100,100, false, true)
+
+G.line(0,0,479,319, true)
+G.line(0,319,479,0, true)
+
+--G.pset(479,318,true)
