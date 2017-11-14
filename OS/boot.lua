88c856881f71b9a70cb930044bebfac7e820fac3
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-11-14 07:07:49 +0200

    Circle Animation

--- OS/boot.lua
+++ OS/boot.lua
@@ -49,19 +49,42 @@ events:registerEvent("love:graphics", function()
   end
 end)]=]
 
-Graphics.clear(true)
+local r = 0
+local inc = true
+local t = 64
+local speed = 1
 
-local P = tonumber(10101010,2)
+while true do
+
+  if inc then
+    r = r + speed
+  else
+    r = r - speed
+  end
+  
+  if inc and r >= t then inc = false
+  elseif r <= 0 and not inc then inc = true
+  end
+  
+  Graphics.clear(true)
+  
+  Graphics.circle(240,160,r)
+  Graphics.circle(240,160,r+8,true)
+  
+  Graphics.flip()
+end
+
+--[[local P = tonumber(10101010,2)
 
 local VRAMLine = 480/8
 local A=0
 
 for Y=0,319 do
   for i=1,VRAMLine do
-    RAM.poke(A,P)
+    RAM.poke(A,math.random(0,255))
     A = A + 1
   end
   Graphics.flip()
-end
+end]]
 
 --Graphics.flip()
